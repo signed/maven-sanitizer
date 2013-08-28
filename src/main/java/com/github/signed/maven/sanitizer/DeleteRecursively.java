630d67f8fbbf8fa305a27c9916206e487c93974b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-08-28 22:57:59 +0200

    Renamed class

--- src/main/java/com/github/signed/maven/sanitizer/DeleteRecursively.java
+++ src/main/java/com/github/signed/maven/sanitizer/DeleteRecursively.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public class DeleteRecursive extends SimpleFileVisitor<Path> {
+public class DeleteRecursively extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         Files.delete(file);
